90a3b9bbb881f4b90b0dde870c4ddb35589503d8
Kennyabby <67015671+Kennyabby@users.noreply.github.com>
2024-12-19 16:15:41 +0100

    new update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Droplets, Facebook, Instagram, Twitter } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white">
+    <footer className="fbg-wave text-white">
       <div className="container mx-auto px-4 py-12">
         <div className="grid md:grid-cols-4 gap-8">
           <div>
