a6ddb93d33b9b2d1c81bde58fc090bddbde7aefc
Kennyabby <67015671+Kennyabby@users.noreply.github.com>
2024-12-23 17:50:08 +0100

    new update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,11 @@
 import React,{useRef, useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 import aboutimg from '../../assets/images/about/grouppic.png'
+import dabaoibi from '../../assets/images/about/daboibi.jpg'
+import guruv from '../../assets/images/about/guruv.jpg'
+import hrm from '../../assets/images/about/hrm.jpg'
 
 const About = ()=>{
     const aboutRef = useRef<HTMLDivElement>(null)
@@ -55,10 +59,40 @@ const About = ()=>{
                     </div>
                     <div className='about-cont-img'><img src={aboutimg} className='about-cont-src'/></div>
                 </div>
-                <div>
-                    <div></div>
-                    <div></div>
+                <div className='ourtech'>
+            <div className='lpage-shape-top'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+                    <path className="lpage-shape-fill" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
+                    c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
+                    c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
+                </svg>
+            </div>
+                <div className='ourtechcov'>
+                    <div className='ourtech-cont'>
+                        <div className='deltrk-title'><span className='inline-col'>MEET</span> OUR TEAM</div>
+                        <div className='deltrk-text'>Founded in 2023, Jazmyne Water Factory uses cutting-edge purification technologies to produce water that meets international standards. Our products are trusted by businesses and organizations for their purity and reliability.</div>
+                    </div>
+                    <div className='tp1'>
+                        <FaChevronLeft className='tparrow1'/>
+                        <FaChevronRight className='tparrow2'/>
+                        <div className='tpcard1'>
+                            <img className='tpcard1-img-src' src={guruv}/>                            
+                            <div className='tpcard1-job'>Factory Manager</div>
+                            <div className='tpcard1-title'>Gurvinda Rahil</div>
+                        </div>                        
+                        <div className='tpcard1'>
+                            <img className='tpcard1-img-src' src={hrm}/>                            
+                            <div className='tpcard1-job'>HR Manager</div>
+                            <div className='tpcard1-title'>Daniel Ameachi</div>
+                        </div>
+                        <div className='tpcard1'>
+                            <img className='tpcard1-img-src' src={dabaoibi}/>                            
+                            <div className='tpcard1-job'>Data Entry</div>
+                            <div className='tpcard1-title'>Daboibi Fubara</div>
+                        </div>
+                    </div>                    
                 </div>
+            </div>
                 <div></div>
             </div>
         </div>
